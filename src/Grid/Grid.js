27b12dca3e444f0ad28e655e61b2f1d63a87850a
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-17 23:11:22 +0530

    Changed linting error node_modules already removed

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -3,10 +3,6 @@ import Cell from '../Cells/Cells'
 import './Grid.css'
 export default class Grid extends Component {
   render() {
-    let gridStyle = {
-      display: 'flex',
-      'flex-direction': 'row'
-    }
     var rows = [];
     for (var i = 0; i < this.props.rows; i++) {
     // note: we add a key prop here to allow react to uniquely identify each
